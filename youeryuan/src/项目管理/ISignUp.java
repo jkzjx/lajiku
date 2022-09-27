bdec82c520f9943edbc92058bb4e7ab52b24bb10
86180 <2730695295@qq.com>
2022-09-28 02:10:38 +0800

    update readme

---
+++ youeryuan/src/项目管理/ISignUp.java
@@ -0,0 +1,15 @@
+package 项目管理;
+	//报名入学接口
+	public interface ISignUp{
+		//打印输出结果
+		public void print();
+		//检查是否有stuType对应的班级名额
+		//如果没有剩余名额，请返回false，否则学生将报名进入该班级并返回true
+		public boolean addStudent(int stuType);
+		//解析命令行输入的参数（格式），如文档描述
+		public static IParams parse()throws Exception{
+			return null;
+		}
+	}
+
+
